7b998a0c7c31533bc49756fc358088ca43971632
7shabir <7shabir@gmail.com>
2020-03-25 23:28:57 +0530

    trace-dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TraceComponent } from './trace/trace.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'trace', component: TraceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
