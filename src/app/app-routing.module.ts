fc29c8ec49d2886655967011ba60ab8ba954b305
AmeenP <ameen.p@target.com>
2020-03-28 00:07:07 +0530

    update app-routing.module.ts - add admin page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TraceComponent } from './trace/trace.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'trace', component: TraceComponent}
+  { path: 'trace', component: TraceComponent},
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
