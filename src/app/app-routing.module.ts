e97df87c8f7e4b0279c3f41e4670aa2b504516b3
AmeenP <ameen.p@target.com>
2020-03-28 12:00:08 +0530

    add routing for contact and privacy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { TraceComponent } from './trace/trace.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
   { path: 'trace', component: TraceComponent},
+  { path: 'privacy', component: PrivacyComponent},
+  { path: 'contact', component: ContactComponent},
   { path: 'admin', component: AdminComponent }
 ];
 
