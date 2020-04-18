0810768ad035016e8acb860ea9c3ee838dbc6e3e
AmeenP <ameen.p@target.com>
2020-04-18 23:59:14 +0530

    update add-patient form

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { RouteMapItem } from '../interfaces/route-map-item';
 import * as firebase from 'firebase/app';
+import { tap } from 'rxjs/operators';
+
 
 import { Patient } from '../interfaces/patient';
 
@@ -10,7 +12,7 @@ import { Patient } from '../interfaces/patient';
   providedIn: 'root'
 })
 export class DatabaseService {
-
+  private locations = [];
   constructor(private firestore: AngularFirestore) { }
 
   addPatient(patient: Patient) {
@@ -24,6 +26,7 @@ export class DatabaseService {
     };
     return this.firestore.collection('patients').add(patientDetails)
       .then((ref) => {
+        console.log(`Added patient with id=${ref.id}`)
         var routeMapItem: RouteMapItem;
         for(routeMapItem of patient.routeMap) {
           this.firestore.collection('patients').doc(ref.id).collection('routeMap').add({
@@ -39,9 +42,59 @@ export class DatabaseService {
           })
           .then((ref2) => {
             console.log(`Added a routeMapItem with id=${ref2.id} to the patient id=${ref.id}`);
+            let db = firebase.firestore();
+            let locRef = db.collection('locations');
+            locRef.where('address', '==', routeMapItem.location).get().then(snapshot => {
+              if (snapshot.empty) {
+
+                this.firestore.collection('locations').add({
+                  district: routeMapItem.district,
+                  address: routeMapItem.location,
+                  geopoint: new firebase.firestore.GeoPoint(routeMapItem.latitude, routeMapItem.longitude)
+                }).then((ref3) => {
+                  console.log(`Added a new location with id=${ref3.id}`);
+                  this.firestore.collection('locations').doc(ref3.id).collection('patients').add({
+                    ref
+                  }).then(() => {
+                    console.log("Added patientRef to the new location")
+                  })
+                  ref2.update({
+                    locRef: ref3
+                  }).then(() => {
+                    console.log("Updated locRef in patient document with new locationRef")
+                  })
+                })
+
+              } else {
+
+                snapshot.forEach(doc => {
+                  console.log(`location already exists with id=${doc.id}`)
+                  ref2.update({
+                    locRef: doc.ref.path
+                  }).then(() => {
+                    console.log("updated locRef in patient with existing location")
+                  })
+                  db.collection('locations').doc(doc.id).collection('patients').add({ref}).then(() => {
+                    console.log("Added patientRef to the existing location")
+                  })
+                })
+                
+              }
+            })
           })
         }
-        console.log(`Added patient with id=${ref.id}`);
       });
+  }
+
+  fetchLocations() {
+    if (this.locations && this.locations.length) {
+      return of(this.locations)
+    } else {
+      return this.firestore.collection('locations', ref => {
+        return ref.orderBy('address')
+      }).valueChanges().pipe(
+        tap((locs) => this.locations = locs)
+      );
+    }
   }
 }
